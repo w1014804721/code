894ac560ee698fbbe78840ea040be84a0395eaef
wjx <1014804721@qq.com>
2018-04-20 21:36:05 +0800

    a

---
+++ deployed.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+scp ./target/apipage /root/apache-tomcat-8.5.11/webapps/ROOT
--- src/main/java/top/wangjingxin/model/vo/UserVO.java
+++ src/main/java/top/wangjingxin/model/vo/UserVO.java
@@ -9,7 +9,7 @@ public class UserVO {
     private Integer check;
     private Integer day = 0;
     private Integer continuous;
-
+    private Integer intergral;
     public void setDay(Integer day) {
         if (day == null) {
             this.day = 0;
