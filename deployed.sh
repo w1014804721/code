5262a5e75ae1da84ecda0e67e2afb779848b6c8a
wjx <1014804721@qq.com>
2018-04-21 20:16:17 +0800

    a

--- deployed.sh
+++ deployed.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-#scp -r ./target/code/WEB-INF/classes/* elas@wangjingxin.top:/home/elas/apache-tomcat-8.5.30/webapps/ROOT/WEB-INF/classes/
 #scp -r ./target/code/* root@wangjingxin.top:/root/apache-tomcat-8.5.30/webapps/ROOT/
-scp -r ./target/code/* utribe@web.utribe.me:/home/tomcat/code
+scp -r ./target/code/WEB-INF/classes/* elas@wangjingxin.top:/home/elas/apache-tomcat-8.5.30/webapps/code/WEB-INF/classes/
+#scp -r ./target/code/WEB-INF/classes/* utribe@web.utribe.me:/home/tomcat/code/WEB-INF/classes/
