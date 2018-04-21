5e9b36f3d869494dd4caef7bef5ae23489684f04
wjx <1014804721@qq.com>
2018-04-21 21:25:25 +0800

    a

--- src/main/java/top/wangjingxin/config/ConfigDomain.java
+++ src/main/java/top/wangjingxin/config/ConfigDomain.java
@@ -9,14 +9,4 @@ import java.util.regex.Pattern;
 public class ConfigDomain {
     private String root;
     private String host;
-
-    public static void main(String[] args) {
-        String s = "<a class='mm' href='http://baidu.com'>test1</a><a class='mm' href='http://baidu.com' id=\"aa\">test2</a>";
-        Pattern p = Pattern.compile("<a\\s+.*?href\\s*=\\s*[\"'](.*?)[\"'].*?>");
-        Matcher matcher = p.matcher(s);
-        while (matcher.find()){
-            System.out.println(matcher.group());
-            System.out.println(matcher.group(1));
-        }
-    }
 }
